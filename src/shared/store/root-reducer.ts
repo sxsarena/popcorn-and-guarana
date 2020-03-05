e86606fae2f08cc5c0032b8445603ab5d4910445
Hemerson Vianna <hemerson.lourenco@gmail.com>
2020-03-05 14:32:10 -0300

    feat: update the service API

--- src/shared/store/root-reducer.ts
+++ src/shared/store/root-reducer.ts
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 
+import movies from './movies';
+
 const createRootReducer = () =>
-  combineReducers({});
+  combineReducers({
+    movies,
+  });
 
 export default createRootReducer;
