1922518f4a86cb36c0ff3cd5941fc42abf2c88d6
Hemerson Vianna <hemerson.lourenco@gmail.com>
2020-03-06 11:14:31 -0300

    feat: add trailer preview and movie details

--- src/shared/components/card/card-component.tsx
+++ src/shared/components/card/card-component.tsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { IMovie } from '../../store/movies/types';
 
 import styles from './card.module.css';
 
 interface OwnProps {
-  movie?: IMovie
+  movie?: IMovie,
 }
 
 const Card: React.FC<OwnProps> = ({ movie }: OwnProps) => {
+  const [trailer, setTrailer] = useState('');
+
   const addDefaultSrc = (evt: React.ChangeEvent<HTMLImageElement>) => {
     evt.target.src = 'https://ingresso-a.akamaihd.net/catalog/Content/img/placeholder-movie-image-4359a6ad34.jpg';
   }
 
+  const handleClick = (evt: React.SyntheticEvent<EventTarget>) => {
+    const trailer = movie?.trailers && movie.trailers[movie.trailers.length-1];
+    if (trailer) {
+      evt.preventDefault();
+
+      setTrailer(trailer.embeddedUrl);
+      window.scrollTo(0, 0);
+    }
+  };
+
+  const handleClose = () => {
+    setTrailer('');
+  }
+
   return (
     <article className={styles.card}>
-      <a className={styles['card-link']} href={`/filme/`}>
+      <a className={styles['card-link']} href={movie?.siteURL} onClick={handleClick}>
         <figure className={styles['card-imageWrap']}>
           <img className={styles['card-image']} src={movie?.thumb} alt={movie?.title} onError={addDefaultSrc} />
           <ul className={styles['card-tagsList']}>
@@ -30,6 +46,16 @@ const Card: React.FC<OwnProps> = ({ movie }: OwnProps) => {
         </figure>
         <strong className={styles['card-title']}>{movie?.title}</strong>
       </a>
+      {trailer !== '' &&
+        <div className={styles['card-trailer']}>
+          <div className={styles['card-trailer-container']}>
+            <div className={styles['card-trailer-videoWrap']}>
+              <a className={styles['card-trailer-link']} href={movie?.siteURL}>Mais detalhes > </a>
+              <button className={styles['card-trailer-close']} onClick={handleClose}>Fechar</button>
+              <iframe width="560" height="315" className={styles['card-trailer-video']} src={trailer}></iframe>
+            </div>
+          </div>
+        </div>}
     </article>
   )
 }
