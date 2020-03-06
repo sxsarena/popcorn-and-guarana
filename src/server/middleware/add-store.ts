b710c66fc10e0a8b25a03c93b739825c16de5293
Hemerson Vianna <hemerson.lourenco@gmail.com>
2020-03-06 03:14:03 -0300

    feat: add the initial state for states and cities

--- src/server/middleware/add-store.ts
+++ src/server/middleware/add-store.ts
@@ -6,7 +6,31 @@ const addStore = (
   res: express.Response,
   next: express.NextFunction
 ): void => {
-  res.locals.store = configureStore({});
+
+  const addCityId = (slug: string) => {
+    switch(slug) {
+      case 'rio-de-janeiro':
+        return '2';
+      default:
+        return '1'; // sao-paulo
+    }
+  }
+
+  res.locals.store = configureStore({
+    initialState : {
+      cities: {
+        currentID: addCityId(_req.query.city),
+        states: [
+          { id: '2', name: 'Rio de Janeiro', slug: 'rio-de-janeiro'},
+          { id: '1', name: 'São Paulo', slug: 'sao-paulo'}
+        ],
+        cities: [
+          { id: '2', name: 'Rio de Janeiro', slug: 'rio-de-janeiro', state: '2'},
+          { id: '1', name: 'São Paulo', slug: 'sao-paulo', state: '1'}
+        ]
+      }
+    }
+  });
   next();
 };
 
