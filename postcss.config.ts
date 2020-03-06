d2bc5a9aeb667b17d7ea264e3c10ba01d48739e7
Hemerson Vianna <hemerson.lourenco@gmail.com>
2020-03-05 23:19:28 -0300

    feat: update postcss configuration to support css variable

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,19 +1,23 @@
 import paths from './config/paths';
 
 export default {
-    plugins: [
-        require('postcss-import')({
-            path: [paths.srcShared, `${__dirname}/node_modules`],
-        }),
-        require('postcss-nested')(),
-        require('postcss-flexbugs-fixes')(),
-        require('autoprefixer')(),
-        require('postcss-custom-properties')(),
-        require('postcss-assets')({
-            basePath: './assets',
-        }),
-        // This is broken.
-        // require('postcss-normalize')(),
-    ],
-    sourceMap: true,
+  plugins: [
+    require('postcss-import')({
+      path: [paths.srcShared, `${__dirname}/node_modules`],
+    }),
+    require('postcss-nested')(),
+    require('postcss-flexbugs-fixes')(),
+    require('postcss-media-variables')(),
+    require('postcss-custom-media')(),
+    require('postcss-css-variables')(),
+    require('postcss-media-variables')(),
+    require('autoprefixer')(),
+    require('postcss-custom-properties')(),
+    require('postcss-assets')({
+      basePath: './assets',
+    }),
+    // This is broken.
+    // require('postcss-normalize')(),
+  ],
+  sourceMap: true,
 };
